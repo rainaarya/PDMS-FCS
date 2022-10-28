7dd85a551a24169ed89ea268bf8fbd3897c3951d
Ashwin R Nair <85173652+AshwinRNair-ARN@users.noreply.github.com>
2022-10-28 23:21:11 +0530

    Partial Commit Of Login Sign-up

--- website/settings.py
+++ website/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
+    'phone_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,9 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = BASE_DIR / 'static'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
 
 
 # Default primary key field type
--- website/urls.py
+++ website/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from signup.views import signaction
 from login.views import loginaction
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/',signaction),
-    path('login/',loginaction),
+    path('',include('login.urls')),
+    path('',include('signup.urls')),
 ]
